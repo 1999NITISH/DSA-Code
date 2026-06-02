feb477039dbd309a96e722917bf107a6a50830d2
Nitish Kumar <91321018+1999NITISH@users.noreply.github.com>
2026-06-02 21:45:34 +0530

    Time: 3 ms (66.67%) | Memory: 47.1 MB (9.92%) - LeetSync

---
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int earliestFinishTime(
+            int[] landStartTime,
+            int[] landDuration,
+            int[] waterStartTime,
+            int[] waterDuration
+    ) {
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < landStartTime.length; i++) {
+            for (int j = 0; j < waterStartTime.length; j++) {
+
+                // Land -> Water
+                int landFinish = landStartTime[i] + landDuration[i];
+
+                int waterStart = Math.max(
+                        landFinish,
+                        waterStartTime[j]
+                );
+
+                int total1 = waterStart + waterDuration[j];
+
+                // Water -> Land
+                int waterFinish = waterStartTime[j] + waterDuration[j];
+
+                int landStart = Math.max(
+                        waterFinish,
+                        landStartTime[i]
+                );
+
+                int total2 = landStart + landDuration[i];
+
+                ans = Math.min(ans, Math.min(total1, total2));
+            }
+        }
+
+        return ans;
+    }
+}
